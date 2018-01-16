99beb9d81162c5891e16796deff7649f048fd274
jasonmcintosh <jasonmcintosh@carfax.com>
2018-01-16 17:27:13 -0600

    Updated with path module, bucket references

--- waflambdafiles.tf
+++ waflambdafiles.tf
@@ -25,36 +25,36 @@ resource "aws_s3_bucket" "WAFLambdaFiles" {
 }
 resource "aws_s3_bucket_object" "LogParserZip" {
     depends_on = ["aws_s3_bucket.WAFLambdaFiles"]
-    bucket = "${var.customer}-waflambdafiles"
+    bucket = "${aws_s3_bucket.WAFLambdaFiles.id}"
     key = "log-parser.zip"
-    source = "files/log-parser/log-parser.zip"
-    etag = "${md5(file("files/log-parser/log-parser.zip"))}"
+    source = "${path.module}/files/log-parser/log-parser.zip"
+    etag = "${md5(file("${path.module}/files/log-parser/log-parser.zip"))}"
 }
 resource "aws_s3_bucket_object" "CustomResourceZip" {
     depends_on = ["aws_s3_bucket.WAFLambdaFiles"]
-    bucket = "${var.customer}-waflambdafiles"
+    bucket = "${aws_s3_bucket.WAFLambdaFiles.id}"
     key = "custom-resource.zip"
-    source = "files/custom-resource/custom-resource.zip"
-    etag = "${md5(file("files/custom-resource/custom-resource.zip"))}"
+    source = "${path.module}/files/custom-resource/custom-resource.zip"
+    etag = "${md5(file("${path.module}/files/custom-resource/custom-resource.zip"))}"
 }
 resource "aws_s3_bucket_object" "AccessHandlerZip" {
     depends_on = ["aws_s3_bucket.WAFLambdaFiles"]
-    bucket = "${var.customer}-waflambdafiles"
+    bucket = "${aws_s3_bucket.WAFLambdaFiles.id}"
     key = "access-handler.zip"
-    source = "files/access-handler/access-handler.zip"
-    etag = "${md5(file("files/access-handler/access-handler.zip"))}"
+    source = "${path.module}/files/access-handler/access-handler.zip"
+    etag = "${md5(file("${path.module}/files/access-handler/access-handler.zip"))}"
 }
 resource "aws_s3_bucket_object" "ReputationListsParserZip" {
     depends_on = ["aws_s3_bucket.WAFLambdaFiles"]
-    bucket = "${var.customer}-waflambdafiles"
+    bucket = "${aws_s3_bucket.WAFLambdaFiles.id}"
     key = "reputation-lists-parser.zip"
-    source = "files/reputation-lists-parser/reputation-lists-parser.zip"
-    etag = "${md5(file("files/reputation-lists-parser/reputation-lists-parser.zip"))}"
+    source = "${path.module}/files/reputation-lists-parser/reputation-lists-parser.zip"
+    etag = "${md5(file("${path.module}/files/reputation-lists-parser/reputation-lists-parser.zip"))}"
 }
 resource "aws_s3_bucket_object" "SolutionHelperZip" {
     depends_on = ["aws_s3_bucket.WAFLambdaFiles"]
-    bucket = "${var.customer}-waflambdafiles"
+    bucket = "${aws_s3_bucket.WAFLambdaFiles.id}"
     key = "solution-helper.zip"
-    source = "files/solution-helper/solution-helper.zip"
-    etag = "${md5(file("files/solution-helper/solution-helper.zip"))}"
+    source = "${path.module}/files/solution-helper/solution-helper.zip"
+    etag = "${md5(file("${path.module}/files/solution-helper/solution-helper.zip"))}"
 }
